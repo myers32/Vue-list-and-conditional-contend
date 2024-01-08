3995b56012b508149aaba9c57d893659ba6c379d
Dawid Osiński <dawidosinski59@gmail.com>
2024-01-08 14:58:12 +0100

    hiding and showing the list

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const app = Vue.createApp({
     return {
       inputData: "",
       tasks: [],
+      taskListIsVisible: true
     };
   },
   methods: {
     addTask() {
       this.tasks.push(this.inputData);
       this.inputData = ""
+    },
+    toggleTaskList() {
+      this.taskListIsVisible = !this.taskListIsVisible;
     }
   }
 });
